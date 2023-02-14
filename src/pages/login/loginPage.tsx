c3e65b7735e78b85e31ed977209cb272a2f622cf
loretdemolas <sebast.loret@gmail.com>
2023-02-14 13:53:57 -0500

    Styling is mostly done.
    logo is a placeholder and will need to be replaced.

--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,5 +1,7 @@
 import { useForm } from 'react-hook-form';
 import './styles.css';
+import logo from './images/weight-plates.png';
+import { Link } from 'react-router-dom';
 
 export function Login() {
   interface LoginInput {
@@ -19,32 +21,49 @@ export function Login() {
 
   return (
     <div className="loginPage">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Email"
-            {...register('Email', {
-              required: true,
-              pattern: /^\S+@\S+$/i,
-            })}
-          />
-          <div>{errors.Email != null && 'Email is required'}</div>
-        </div>
-        <div className="form-control">
-          <input
-            type="password"
-            placeholder="Password"
-            {...register('Password', {
-              required: true,
-            })}
-          />
-          <div>{errors.Password != null && 'Password is required'}</div>
-        </div>
-        <div className="form-control">
-          <button type="submit">Login</button>
-        </div>
-      </form>
+      <div className="loginBox">
+        <h1 className="title">Login</h1>
+        <img className="logo" src={logo} />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Email"
+              {...register('Email', {
+                required: true,
+                pattern: /^\S+@\S+$/i,
+              })}
+            />
+            <div className="error-message">
+              {errors.Email != null && 'Email is required'}
+            </div>
+          </div>
+          <div>
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Password"
+              {...register('Password', {
+                required: true,
+              })}
+            />
+            <div className="error-message">
+              {errors.Password != null && 'Password is required'}
+            </div>
+            <p className="forgotPassword">Forgot password?</p>
+          </div>
+          <button className="submit" type="submit">
+            Login
+          </button>
+          <div className="register">
+            <p>Or Sign Up Using</p>
+            <Link to="/register">
+              <a> SIGN UP</a>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
