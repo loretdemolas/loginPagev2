856c6728c49f98337128c33fa13bab538fcc7a9a
loretdemolas <sebast.loret@gmail.com>
2023-02-14 11:46:27 -0500

    Login Page setup is done.
    Need todo css and auth and push data to redux

--- README.md
+++ README.md
@@ -3,22 +3,22 @@
 Dependencies
 
 Node-Fetch instead of Axios
-    <https://github.com/node-fetch/node-fetch/tree/2.x#readme>
+<https://github.com/node-fetch/node-fetch/tree/2.x#readme>
 Tailwind
-    <https://tailwindcss.com/docs/installation>
+<https://tailwindcss.com/docs/installation>
 react hook form
-    <https://legacy.react-hook-form.com/get-started>
+<https://legacy.react-hook-form.com/get-started>
 React
-    <https://github.com/facebook/react>
+<https://github.com/facebook/react>
 React-dom
-    <https://github.com/facebook/react/tree/main/packages/react-dom>
+<https://github.com/facebook/react/tree/main/packages/react-dom>
 React-router-dom
-    <https://reactrouter.com/en/main>
+<https://reactrouter.com/en/main>
 Zod instead of yup for type script support
 <https://github.com/colinhacks/zod>
 Framer :for implementing animation and user feedback
-    <https://www.framer.com/motion/examples/>
-*devDependencies
+<https://www.framer.com/motion/examples/>
+\*devDependencies
 Vite
 <https://vitejs.dev/guide/>
 typescript
--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,3 +1,50 @@
+import { useForm } from 'react-hook-form';
+import './styles.css';
+
 export function Login() {
-  return <h1>hello world</h1>;
+  interface LoginInput {
+    Email: string;
+    Password: string;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInput>();
+  const onSubmit: SubmitHandler<LoginInput> = (data) => {
+    console.log(data);
+  };
+  console.log(errors);
+
+  return (
+    <div className="loginPage">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Email"
+            {...register('Email', {
+              required: true,
+              pattern: /^\S+@\S+$/i,
+            })}
+          />
+          <div>{errors.Email != null && 'Email is required'}</div>
+        </div>
+        <div className="form-control">
+          <input
+            type="password"
+            placeholder="Password"
+            {...register('Password', {
+              required: true,
+            })}
+          />
+          <div>{errors.Password != null && 'Password is required'}</div>
+        </div>
+        <div className="form-control">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
